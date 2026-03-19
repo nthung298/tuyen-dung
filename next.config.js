4a0c1dac23595cfa10273686b0accbde1aed8159
ruanjinxiong <ruanjinxiong@yahoo.com>
2026-03-19 20:28:13 +0700

    MVP fix 1

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
